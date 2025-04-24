84923f8423d48a29cd1dcd062351a72a59c7d3c1
r8023 <tracy880203@gmail.com>
2025-04-24 23:13:26 +0800

    Update general.py

--- python/general.py
+++ python/general.py
@@ -84,14 +84,14 @@ def get_all_products(base_url, headers, sleep_sec=0.5):
                     published_dt = datetime.strptime(published_at, "%Y-%m-%dT%H:%M:%S%z")
                     if published_dt > datetime.now(published_dt.tzinfo):
                         is_future = True
-                        publish_at = dt.strftime("%Y/%m/%d %H:%M")
+                        publish_at = published_dt.strftime("%Y/%m/%d %H:%M")
                 except Exception:
                     pass
 
             created_dt = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%S%z")
             if created_dt > datetime.now(created_dt.tzinfo):
                 is_future = True
-                created_at = dt.strftime("%Y/%m/%d %H:%M")
+                created_at = created_at.strftime("%Y/%m/%d %H:%M")
 
             product = {
                 "id": p["id"],
